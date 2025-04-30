d385090ed4ea03fefad6513f08f4185a56b5113f
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-04-30 12:52:52 -0400

    Layout & Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Layout from "./components/Layout";
+import Hero from "./components/sections/Hero";
+
 function App() {
-    return <></>;
+    return (
+        <Layout title="LaunchBase">
+            <Hero />
+        </Layout>
+    );
 }
 
 export default App;
