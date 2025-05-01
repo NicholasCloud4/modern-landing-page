8e503c694418ea64528acda616d7d68e09342366
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-05-01 11:07:32 -0400

    Created Brands section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Layout from "./components/Layout";
+import { Brands } from "./components/sections/Brands";
 import Hero from "./components/sections/Hero";
 
 function App() {
     return (
         <Layout title="LaunchBase">
             <Hero />
-            {/* <Brands /> */}
+            <Brands />
         </Layout>
     );
 }
